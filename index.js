862d07185c263da21bae325f95d530ecbe59aa67
Brian Kimutai <brian.kimutai@student.moringaschool.com>
2022-06-10 15:43:43 +0300

    commit tree 1a93ca2984c781ff9ec80daa9cd29ec05b177a1e

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(drivers){
+return drivers.slice(0,2)
+}
+function returnLastTwoDrivers(drivers){
+
+return drivers.slice(2)
+
+}
+// let   selectingDrivers=[['Amari', 'Mo',],'Antonia', 'Nuru',]
+const selectingDrivers = [returnFirstTwoDrivers,
+     returnLastTwoDrivers];
+
+
+
+
+function createFareMultiplier(value) {
+    return function(fare=5) {
+        return fare * value;
+    }
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(driversArray, fn) {
+    return fn(driversArray);
+}
